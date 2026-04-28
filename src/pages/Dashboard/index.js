492336584cd0d6f4a44002e3384a9fc995282dc6
Praveen <praveennandipati@gmail.com>
2026-04-28 15:53:21 +0530

    commit first files

---
+++ src/pages/Dashboard/index.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { Container } from "reactstrap";
+
+//Import Breadcrumb
+import Breadcrumbs from "../../components/Common/Breadcrumb";
+
+const Dashboard = () => {
+
+  document.title=" Dashboard | Minible - Responsive Bootstrap 5 Admin Dashboard"
+
+  return (
+    <React.Fragment>
+      <div className="page-content">
+        <Container fluid>
+          <Breadcrumbs title="Minible" breadcrumbItem="Dashboard" />
+        </Container>
+      </div>
+    </React.Fragment>
+  );
+};
+
+export default Dashboard;
